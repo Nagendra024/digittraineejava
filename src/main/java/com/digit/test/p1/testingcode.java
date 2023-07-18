0e043507de5d93c4e486405420a6404e89d153ba
Nagendra.S <Nagendra.S@LDIGITBGL-L0204.godigit.corp>
2023-07-18 12:46:07 +0530

    changes

--- src/main/java/com/digit/test/p1/testingcode.java
+++ src/main/java/com/digit/test/p1/testingcode.java
@@ -3,6 +3,10 @@ package com.digit.test.p1;
 public class testingcode {
 	public static void main(String[] args) {
 		System.out.println("hi from nagendra");
+		System.out.println("hi from nagendra");
+		
+		System.out.println("hi from koushik");
+		System.out.println("hi from koushik");
 	}
 
 }
